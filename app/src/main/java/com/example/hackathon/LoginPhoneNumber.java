46ad05e9adbf5d15f49fbff20f5cef1766d5fc6d
Mangalya Shah <mangalya2110@gmail.com>
2024-03-01 22:33:39 +0530

    add: Add LoginPhoneNumber.java add: Add activity_login_phone_number.xml

---
+++ app/src/main/java/com/example/hackathon/LoginPhoneNumber.java
@@ -0,0 +1,22 @@
+package com.example.hackathon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.hbb20.CountryCodePicker;
+
+public class LoginPhoneNumber extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_phone_number);
+
+
+    }
+}
--- app/src/main/java/com/example/hackathon/MainActivity.java
+++ app/src/main/java/com/example/hackathon/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent=new Intent(this,HomeActivity.class);
+        Intent intent=new Intent(this, LoginPhoneNumber.class);
 
         Runnable runnable=new Runnable() {
             @Override
