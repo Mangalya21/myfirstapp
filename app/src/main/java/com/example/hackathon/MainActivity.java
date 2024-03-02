b88514567a0760c5010ba85d8cc08cc6b10ff91b
lekhan patel <lekhanpatel2@gmail.com>
2024-03-02 17:39:17 +0530

    Change:LOGO

--- app/src/main/java/com/example/hackathon/MainActivity.java
+++ app/src/main/java/com/example/hackathon/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         Handler handler=new Handler();
-        handler.postDelayed(runnable,3000);
+        handler.postDelayed(runnable,2000);
 
     }
 }
