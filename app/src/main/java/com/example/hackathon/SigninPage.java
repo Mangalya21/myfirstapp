b64b7974adf2478ccf28404bed6c4390aff5bb35
Mangalya Shah <mangalya2110@gmail.com>
2024-03-02 22:21:21 +0530

    chore: Update SigninPage.java chore: Update activity_signin_page.xml

--- app/src/main/java/com/example/hackathon/SigninPage.java
+++ app/src/main/java/com/example/hackathon/SigninPage.java
@@ -2,13 +2,27 @@ package com.example.hackathon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SigninPage extends AppCompatActivity {
 
+    Button userBtn = findViewById(R.id.user_btn);
+    Button bloodBankBtn = findViewById(R.id.blood_bank_btn);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin_page);
+
+        userBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SigninPage.this,LoginPhoneNumberActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
